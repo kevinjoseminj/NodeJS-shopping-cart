695ed6a240900bfd2994cd350b8331cf17b36808
Kevin Jose Minj <kevinjoseminj98@gmail.com>
2020-11-21 15:24:23 +0530

    Payment gateway integrated

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,16 +1,22 @@
 var db = require('../config/connection')
 var collection = require('../config/collections')
 const bcrypt = require('bcrypt')
-var objectId=require('mongodb').ObjectID
+var objectId = require('mongodb').ObjectID
+const Razorpay = require('razorpay')
+var instance = new Razorpay({
+    key_id: 'rzp_test_fqeeLBmxwPCXMC',
+    key_secret: 'T0saj21AKiU5vv3ypPpMWMdv',
+});
 
-module.exports={
-    doSignup:(userData)=>{
+
+module.exports = {
+    doSignup: (userData) => {
         return new Promise(async (resolve, reject) => {
-        userData.Password = await bcrypt.hash(userData.Password, 10)
-        db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data) => {
-            resolve(data.ops[0])
+            userData.Password = await bcrypt.hash(userData.Password, 10)
+            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data) => {
+                resolve(data.ops[0])
+            })
         })
-    })
 
 
     },
@@ -39,7 +45,7 @@ module.exports={
             }
         })
     },
-    addToCart:(proId,userId) => {
+    addToCart: (proId, userId) => {
         let proObj = {
             item: objectId(proId),
             quantity: 1
@@ -49,8 +55,8 @@ module.exports={
             if (userCart) {
                 let proExist = userCart.products.findIndex(product => product.item == proId)
                 console.log(proExist);
-                if (proExist !=-1) {
-                    db.get().collection(collection.CART_COLLECTION).updateOne({'products.item': objectId(proId) },
+                if (proExist != -1) {
+                    db.get().collection(collection.CART_COLLECTION).updateOne({ 'products.item': objectId(proId) },
                         {
                             $inc: { 'products.$.quantity': 1 }
                         }).then(() => {
@@ -116,11 +122,11 @@ module.exports={
             resolve(cartItems)
         })
     },
-    getCartCount:(userId) => {
-        return new Promise(async (resolve,reject) => {
-            let count=0
-            let cart = await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
-            if(cart){
+    getCartCount: (userId) => {
+        return new Promise(async (resolve, reject) => {
+            let count = 0
+            let cart = await db.get().collection(collection.CART_COLLECTION).findOne({ user: objectId(userId) })
+            if (cart) {
                 count = cart.products.length
             }
             resolve(count)
@@ -131,33 +137,33 @@ module.exports={
         details.quantity = parseInt(details.quantity)
 
         return new Promise((resolve, reject) => {
-            if (details.count==-1 && details.quantity==1){
-            db.get().collection(collection.CART_COLLECTION).updateOne({ _id:objectId(details.cart)},
-                {
-                    $pull: {products:{item:objectId(details.product)}}
-                }
+            if (details.count == -1 && details.quantity == 1) {
+                db.get().collection(collection.CART_COLLECTION).updateOne({ _id: objectId(details.cart) },
+                    {
+                        $pull: { products: { item: objectId(details.product) } }
+                    }
                 ).then((response) => {
                     console.log(response);
                     resolve({ removeProduct: true })
                 })
-            }else{
-            db.get().collection(collection.CART_COLLECTION)
-                .updateOne({ _id:objectId(details.cart),'products.item':objectId(details.product)},
-                {
-                    $inc: { 'products.$.quantity': details.count }
-                }
-                ).then((response) => {
-                    resolve({status:true})
-                })
+            } else {
+                db.get().collection(collection.CART_COLLECTION)
+                    .updateOne({ _id: objectId(details.cart), 'products.item': objectId(details.product) },
+                        {
+                            $inc: { 'products.$.quantity': details.count }
+                        }
+                    ).then((response) => {
+                        resolve({ status: true })
+                    })
             }
         })
     },
-    getTotalAmount:(userId)=>{
+    getTotalAmount: (userId) => {
         return new Promise(async (resolve, reject) => {
             let total = await db.get().collection(collection.CART_COLLECTION).aggregate([
                 {
-                    $match:{user:objectId(userId)}
-    
+                    $match: { user: objectId(userId) }
+
                 },
                 {
                     $unwind: '$products'
@@ -166,7 +172,7 @@ module.exports={
                         item: '$products.item',
                         quantity: '$products.quantity'
                     }
-    
+
                 },
                 {
                     $lookup: {
@@ -182,9 +188,9 @@ module.exports={
                     }
                 },
                 {
-                    $group:{
-                        _id:null,
-                        total:{$sum:{$multiply:['$quantity','$product.Price']}}
+                    $group: {
+                        _id: null,
+                        total: { $sum: { $multiply: ['$quantity', '$product.Price'] } }
                     }
                 }
             ]).toArray()
@@ -192,39 +198,101 @@ module.exports={
             resolve(total[0].total)
         })
     },
-    placeOrder:(order,products,total)=>{
-        return new Promise(async(resolve,reject)=>{
-            console.log(order,products,total);
-            let status = order['payment-method']==='COD'?'placed':'pending'
-            let orderObj={
-                deliveryDetails:{
-                    mobile:order.mobile,
-                    address:order.address,
-                    pincode:order.pincode
+    placeOrder: (order, products, total) => {
+        return new Promise(async (resolve, reject) => {
+            console.log(order, products, total);
+            let status = order['payment-method'] === 'COD' ? 'placed' : 'pending'
+            let orderObj = {
+                deliveryDetails: {
+                    mobile: order.mobile,
+                    address: order.address,
+                    pincode: order.pincode
                 },
-                userId:objectId(order.userId),
-                paymentMethod:order['payment-method'],
-                products:products,
-                totalAmount:total,
-                status:status,
-                date:new Date()
-    
+                userId: objectId(order.userId),
+                paymentMethod: order['payment-method'],
+                products: products,
+                totalAmount: total,
+                status: status,
+                date: new Date()
+
             }
-    
-            db.get().collection(collection.ORDER_COLLECTION).insertOne(orderObj).then((response)=>{
-                db.get().collection(collection.CART_COLLECTION).remove({user:objectId(order.userId)})
-                resolve()
+
+            db.get().collection(collection.ORDER_COLLECTION).insertOne(orderObj).then((response) => {
+                db.get().collection(collection.CART_COLLECTION).remove({ user: objectId(order.userId) })
+                console.log("Order id:", response.ops[0]._id);
+                resolve(response.ops[0]._id)
             })
-            
-    
+
+
         })
-    
+
     },
-    getCartProductList:(userId)=>{
-        return new Promise(async (resolve,reject)=>{
-            let cart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+    getCartProductList: (userId) => {
+        return new Promise(async (resolve, reject) => {
+            let cart = await db.get().collection(collection.CART_COLLECTION).findOne({ user: objectId(userId) })
             console.log(cart);
             resolve(cart.products)
         })
-    }  
+    },
+    getUserOrders: (userId) => {
+        return new Promise(async (resolve, reject) => {
+            console.log(userId);
+            let orders = await db.get().collection(collection.ORDER_COLLECTION).find({ userId: objectId(userId) }).toArray()
+            console.log(orders);
+            resolve(orders)
+        })
+    },
+    getOrderProducts: (orderId) => {
+        return new Promise(async (resolve, reject) => {
+            let orderItems = await db.get().collection(collection.ORDER_COLLECTION).aggregate([
+                {
+                    $match: { _id: objectId(orderId) }
+                },
+                {
+                    $unwind: '$products'
+                },
+                {
+                    $project: {
+                        item: '$products.item',
+                        quantity: '$products.quantity'
+                    }
+                },
+                {
+                    $lookup: {
+                        from: collection.PRODUCT_COLLECTION,
+                        localField: 'item',
+                        foreignField: '_id',
+                        as: 'product'
+                    }
+                },
+                {
+                    $project: {
+                        item: 1, quantity: 1, product: { $arrayElemAt: ['$product', 0] }
+                    }
+                }
+
+            ]).toArray()
+            console.log(orderItems);
+            resolve(orderItems)
+        })
+    },
+    generateRazorpay: (orderId, totalPrice) => {
+        return new Promise((resolve, reject) => {
+            var options = {
+                amount: total,  // amount in the smallest currency unit
+                currency: "INR",
+                receipt: "" + orderId
+            };
+            instance.orders.create(options, function (err, order) {
+                if (errr) {
+                    console.log(err);
+
+                } else {
+                    console.log("NEw Order :", order);
+                    resolve(order)
+                }
+            });
+
+        })
+    }
 } 
--- routes/user.js
+++ routes/user.js
@@ -2,27 +2,27 @@ var express = require('express');
 var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelper = require('../helpers/user-helpers')
-const verifyLogin=(req,res,next)=>{
-  if(req.session.loggedIn){
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
     next()
-   }else{
-  res.redirect('/login')
-   }
+  } else {
+    res.redirect('/login')
+  }
 }
 
 /* GET home page. */
 
 
-router.get('/', async function(req, res, next) {
-  let user=req.session.user
+router.get('/', async function (req, res, next) {
+  let user = req.session.user
   console.log(user);
-  let cartCount=null
-  if(req.session.user){
-  cartCount=await userHelper.getCartCount(req.session.user._id)
+  let cartCount = null
+  if (req.session.user) {
+    cartCount = await userHelper.getCartCount(req.session.user._id)
   }
-  productHelpers.getAllProducts().then((products)=>{
-    
-    res.render( 'user/view-products', { products,user,cartCount})
+  productHelpers.getAllProducts().then((products) => {
+
+    res.render('user/view-products', { products, user, cartCount })
   })
 });
 
@@ -30,87 +30,118 @@ router.get('/', async function(req, res, next) {
 
 
 
-router.get('/login',(req,res)=>{
-  if(req.session.loggedIn){
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
     res.redirect('/')
-  }else{
-    res.render('user/login',{"loginErr":req.session.loginErr})
-    req.session.loginErr=false
+  } else {
+    res.render('user/login', { "loginErr": req.session.loginErr })
+    req.session.loginErr = false
   }
 })
 
-router.get('/signup',(req,res)=>{
+router.get('/signup', (req, res) => {
   res.render('user/signup')
 })
 
 
-router.post('/signup',(req,res)=>{
-  userHelper.doSignup(req.body).then((response)=>{
+router.post('/signup', (req, res) => {
+  userHelper.doSignup(req.body).then((response) => {
     console.log(response);
-    req.session.loggedIn=true
-    req.session.user=response
-    res.redirect('/') 
-  }) 
-}) 
-
-router.post('/login',(req,res)=>{
-  userHelper.doLogin(req.body).then((response)=>{
-    if(response.status){
-      req.session.loggedIn=true
-      req.session.user=response.user
-      res.redirect('/')
-    }else{
-      req.session.loginErr="Invalid username or password"
-      res.redirect('/login')
-    } 
+    req.session.loggedIn = true
+    req.session.user = response
+    res.redirect('/')
   })
 })
 
-router.get('/logout',(req,res)=>{
+router.post('/login', (req, res) => {
+  userHelper.doLogin(req.body).then((response) => {
+    if (response.status) {
+      req.session.loggedIn = true
+      req.session.user = response.user
+      res.redirect('/')
+    } else {
+      req.session.loginErr = "Invalid username or password"
+      res.redirect('/login')
+    }
+  })
+})
+
+router.get('/logout', (req, res) => {
   req.session.destroy()
   res.redirect('/')
 })
 
-router.get('/cart', verifyLogin,async (req,res)=>{
-  let products=await userHelper.getCartProducts(req.session.user._id)
-  let totalValue=await userHelper.getTotalAmount(req.session.user._id)
+router.get('/cart', verifyLogin, async (req, res) => {
+  let products = await userHelper.getCartProducts(req.session.user._id)
+  let totalValue = await userHelper.getTotalAmount(req.session.user._id)
   console.log(products);
-  console.log('***'+req.session.user_id);
-  res.render('user/cart',{products,user:req.session.user._id,totalValue})
+  console.log('***' + req.session.user_id);
+  res.render('user/cart', { products, user: req.session.user._id, totalValue })
 })
 
 
-router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
+router.get('/add-to-cart/:id', verifyLogin, (req, res) => {
   console.log("api call");
-  userHelper.addToCart(req.params.id,req.session.user._id).then(()=>{
-   res.json({status:true})
+  userHelper.addToCart(req.params.id, req.session.user._id).then(() => {
+    res.json({ status: true })
   })
 
 })
 
-router.post('/change-product-quantity',(req,res,next)=>{
+router.post('/change-product-quantity', (req, res, next) => {
   console.log(req.body);
-  userHelper.changeProductQuantity(req.body).then(async(response)=>{
-    response.total=await userHelper.getTotalAmount(req.body.user)
+  userHelper.changeProductQuantity(req.body).then(async (response) => {
+    response.total = await userHelper.getTotalAmount(req.body.user)
     res.json(response)
 
   })
 })
 
-router.get('/place-order',verifyLogin, async (req,res)=>{
+router.get('/place-order', verifyLogin, async (req, res) => {
   let total = await userHelper.getTotalAmount(req.session.user_id)
-  res.render('user/place-order',{total,user:req.session.user})
+  res.render('user/place-order', { total, user: req.session.user })
 })
 
-router.post('/place-order',async (req,res)=>{
+router.post('/place-order', async (req, res) => {
   console.log(req.body);
-  let products=await userHelper.getCartProductList(req.body.userId)
-  let totalPrice=await userHelper.getTotalAmount(req.body.userId)
-  userHelper.placeOrder(req.body,products,totalPrice).then((response)=>{
-    res.json({status:true})
-  })
+  let products = await userHelper.getCartProductList(req.body.userId)
+  let totalPrice = await userHelper.getTotalAmount(req.body.userId)
+  userHelper.placeOrder(req.body, products, totalPrice).then((orderId) => {
+    console.log(orderId);
+    if (req.body['payment-method'] === 'COD') {
+      res.json({ codSuccess: true })
+
+    } else {
+      userHelper.generateRazorpay(orderId, totalPrice).then((respons) => {
+        res.json(response)
+
+      })
+
+
+
+    }
 
   })
+  console.log(req.body);
+})
+
+router.get('/order-success', (req, res) => {
+  res.render('user/order-success', { user: req.session.user })
+})
+
+router.get('/orders', async (req, res) => {
+  let orders = await userHelper.getOrderProducts(req.params.id)
+  res.render('user/orders', { user: req.session, user, orders })
+})
+
+router.get('/view-order-products/:id', async (req, res) => {
+  let products = await userHelper.getOrderProducts(req.params.id)
+  res.render('user/view-order-products', { user: req.session.user, products })
+})
+
+router.post('verify-payment', (req, res) => {
+  console.log(req.body);
+})
 
 
 
